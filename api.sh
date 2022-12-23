323735027890019ccfbbfcc8c3bacee5d45f09ed
Jesse Egbosionu <j3bsie@gmail.com>
2022-12-23 15:21:26 +0100

    chore(cleanup): Removed files

--- api.sh
+++ api.sh
@@ -16,6 +16,7 @@ then
     echo "Starting Production"
     uvicorn main:app --workers $WORKER_COUNT --host 0.0.0.0 --port 8022
 elif [[ "$ENV" == "app-runner" ]]
+then
     echo "Starting Application on App Runner"
     echo "Running Database Migrations."
     alembic upgrade head
